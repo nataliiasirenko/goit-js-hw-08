e1ff87bf268a865723e3a50dcad25a28881d28ea
nataliiasirenko <sirenko.nataliia@hotmail.com>
2022-12-14 10:01:13 +0000

    update js file

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,8 +1,5 @@
-// Описаний в документації
 import SimpleLightbox from 'simplelightbox';
-// Додатковий імпорт стилів
 import 'simplelightbox/dist/simple-lightbox.min.css';
-import SimpleLightbox from 'simplelightbox/dist/simple-lightbox.esm'; // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
