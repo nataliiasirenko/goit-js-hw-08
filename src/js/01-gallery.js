1f6f2a5c73d2925fe03bf2ced7c48b1391eb4c26
nataliiasirenko <sirenko.nataliia@hotmail.com>
2022-12-14 15:55:54 +0000

    update gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,42 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const makeGalerryItemsMarkup = ({ preview, original, description }) => {
+  return `
+    <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>`;
+};
+
+const makeGalerryitems = galleryItems.map(makeGalerryItemsMarkup).join('');
+const galleryEl = document.querySelector('.gallery');
+galleryEl.insertAdjacentHTML('beforeend', makeGalerryitems);
+galleryEl.addEventListener('click', handleImageClick);
+
+function handleImageClick(e) {
+  e.preventDefault();
+  if (!e.target.classList.contains('gallery__image')) {
+    return;
+  }
+  const instance = basicLightbox.create(`
+    <img src="${e.target.dataset.source}" width -"800" height-"600">
+  `);
+  instance.show();
+
+  galleryEl.addEventListener('keydown', e => {
+    if (e.code === 'Escape') {
+      instance.close();
+    }
+  });
+}
