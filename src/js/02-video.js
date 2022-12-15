779bc37850be8c69129c3ca43ebf3199f1ebd929
nataliiasirenko <sirenko.nataliia@hotmail.com>
2022-12-15 20:39:19 +0000

    update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,20 +1,17 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 // const player = new Player('handstick', {
 //   id: 19231868,
 //   width: 640,
 // });
 
-// player.on('play', function () {
-//   console.log('played the video!');
-// });
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
+player.setCurrentTime(localStorage.getItem(`setCurrentTime(|)`) || 0);
 
-player.on('play', function () {
-  console.log('played the video!');
-});
+player.on('timeupdate', throttle(onPlay, 1000));
 
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
+function onPlay({ seconds }) {
+  localStorage.setIte, (`videoplayer-current-time`, seconds);
+}
